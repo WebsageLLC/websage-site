c06c3c0717d51cceda84be2ce7037c4b29904112
Crothos <s.g.totten@gmail.com>
2023-06-29 14:21:49 -0500

    home component text tweak

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
 
 
                <div className=''>
-                <p className='text-[#edf5e1] [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] text-xl'>Welcome to</p>
+                <p className='text-[#05386B] [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] text-xl'>Welcome to</p>
                 <h1> <span className='text-4xl sm:text-7xl font-bold text-[#379683]'> Web</span> <span className='text-4xl sm:text-7xl font-bold text-[#379683]'>Sage</span><span className='text-4xl sm:text-7xl text-[#edf5e1]'>llc</span></h1>
                 <h1> <span className='text-4xl sm:text-7xl font-bold text-[#379683]'> Web</span> <span className='text-4xl sm:text-7xl  text-[#edf5e1]'>development </span></h1>
-                <p className='text-xl text-[#edf5e1] py-4 max-w-[700px] [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)]'>Whether you need a simple page or a full site, we're here to help!</p>
+                <p className='text-xl text-[#05386B] py-4 max-w-[700px] [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]'>Whether you need a simple page or a full site, we're here to help!</p>
                 <div className='flex flex-row'>
                 <Link to="about" smooth={true} duration={500}>
                     <button className='text-white group border-2 px-6 py-3 mx-5 my-2 flex items-center hover:bg-[#05386b] hover:border-[#05386b]'>About Us 
